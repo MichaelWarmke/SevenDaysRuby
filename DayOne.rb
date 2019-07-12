ab2ec7f5f3f40b54d7a133ccd906af630ddbfcab
Michael Warmke <mikew670@gmail.com>
2019-07-11 21:44:39 -0500

    Day One Ruby

---
+++ DayOne.rb
@@ -0,0 +1,28 @@
+
+puts "Hello World"
+
+hiRuby = "Hello, Ruby."
+
+#puts hiRuby.methods
+
+puts hiRuby.rindex("Ruby.")
+
+x = 10
+until x==0
+    puts "Michael Warmke"
+    x -= 1
+end
+
+#Inline
+(1..10).each { |n| puts "Michael Warmke"}
+
+index = 1
+while index <= 10
+    puts "This is sentence number #{index}"
+    index += 1
+end
+
+#Inline
+(1..10).each { |n| puts "this is sentence number #{n}"}
+
+
